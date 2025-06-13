4cdfca21a09173198f9314b1985ebb0dd9311c3b
Sigilante <neal@zorp.io>
2025-06-13 18:25:14 -0500

    Post almost working jockt.

---
+++ crates/jockt/hoon/lib/jock.hoon
@@ -0,0 +1,1 @@
+../../../../common/hoon/lib/jock.hoon
--- crates/jockt/main.rs
+++ crates/jockt/main.rs
@@ -19,6 +19,13 @@ struct ExecCli {
 
     #[command(subcommand)]
     command: Command,
+
+    #[arg(
+        long = "import-dir",
+        help = "Supply a path for library imports",
+        num_args = 1
+    )]
+    lib_path: Option<String>,
 }
 
 #[derive(Parser, Debug)]
@@ -65,6 +72,59 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await?;
 
     boot::init_default_tracing(&cli.boot.clone());
+
+    // Load libraries from path if provided.
+    let lib_path = cli.lib_path.unwrap_or("lib_path".to_string());
+    // Get names of all Hoon and Jock files in that directory.
+    // let mut lib_names = Vec::new();
+    let mut lib_texts:Vec<(Atom,Atom)> = Vec::new();
+    if let Ok(entries) = std::fs::read_dir(lib_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if let Some(ext) = path.extension() {
+                    if ext == "hoon" || ext == "jock" || ext == "txt" {  // XXX kludge for now on txt
+                        if let Some(stem) = path.file_stem() {
+                            if let Some(stem_str) = stem.to_str() {
+                                let lib_name = Atom::from_value(&mut slab, stem_str.to_string())
+                                    .unwrap()
+                                    .as_noun()
+                                    .as_atom()
+                                    .unwrap();
+                                // lib_names.push(lib_name);
+                                // Read file content.
+                                let lib_text = std::fs::read_to_string(&path)
+                                    .expect("Unable to read library file");
+                                let _lib_text = Atom::from_value(&mut slab, lib_text.clone())
+                                    .unwrap()
+                                    .as_noun()
+                                    .as_atom()
+                                    .unwrap();
+                                // lib_texts.push(T(&mut slab, &[lib_name, _lib_text]));
+                                lib_texts.push((lib_name, _lib_text));
+                                println!("Loaded library: {}", stem_str);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("Found {} library files", lib_texts.len());
+    if (lib_texts.len() > 0) {
+        let tuple = vec_to_hoon_tuple_list(&mut slab, lib_texts);
+        let tas = make_tas(&mut slab, "load-libs");
+        create_poke(&[
+            D(tas!(b"jock")),
+            T(&mut slab, &[tas, tuple])
+        ])
+        
+        nockapp
+            .add_io_driver(one_punch_driver(poke, Operation::Poke))
+            .await;
+
+        nockapp.run().await?;
+    }
 
     let poke = match cli.command {
         Command::Exec { n } => {
@@ -134,4 +194,16 @@ fn create_poke(args: &[Noun]) -> NounSlab {
     let copy_root = T(&mut slab, args);
     slab.copy_into(copy_root);
     slab
+}
+
+#[inline(always)]
+pub fn vec_to_hoon_tuple_list(slab: &mut NounSlab, vec: Vec<(Atom,Atom)>) -> Noun {
+    let mut list = D(0);
+    for (a,b) in vec.iter().rev() {
+        let n1 = a.as_noun();
+        let n2 = b.as_noun();
+        let tuple = T(slab, &[n1, n2]);
+        list = T(slab, &[tuple, list]);
+    }
+    list
 }
